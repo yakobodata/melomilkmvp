4e6c15ee5b8c56d6df3824d4119a27426a09cede
wamani jacob <120217843+yakobodata@users.noreply.github.com>
2023-07-22 13:16:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Signin(),
+));
+
+class Signin extends StatelessWidget {
+int _success = 1;
+String _userEmail = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Melo Milk'),
+        centerTitle: true,
+
+      ),
+      body:Column(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15, 110, 0, 0),
+                  child: Text("Welcome",
+                  style: TextStyle(
+                    fontSize: 40,fontWeight: FontWeight.bold
+                  ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35,left: 20,right: 30,),
+            child: Column(
+              children: <Widget>[
+                TextField(
+
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    )
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText : 'PASSWORD',
+                    labelStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:BorderSide(color: Colors.green),
+                    )
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0,),
+                Container(
+                  alignment: Alignment(1,0),
+                  padding: EdgeInsets.only(top: 15,left: 20),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        decoration: TextDecoration.underline
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(_success == 1 ? '': (_success == 2 ? 'Successfully signed in '+ _userEmail : 'Sign in failed'),
+                    style: TextStyle(color: Colors.red),
+                    )
+                ),
+                SizedBox(height: 40,),
+                Container(
+                  height: 40,
+                  child: Material(
+                  borderRadius: BorderRadius.circular(20),
+                  shadowColor: Colors.greenAccent,
+                  color: Colors.black,
+                  elevation: 7,
+                  child: GestureDetector(
+
+                      child:Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat'
+                          ),
+                        ),
+                      )
+                  ),
+                  ),
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: (){
+                        Navigator.of(context).pushNamed('');
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+
+          )
+        ],
+      )
+
+    );
+  }
+}
+
+
